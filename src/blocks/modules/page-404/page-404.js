9afbba905d7d2fffbd9a9e1549985116d27adf62
zukaaman <zukaaman@gmail.com>
2021-04-01 18:31:27 +0400

    1.4.21

---
+++ src/blocks/modules/page-404/page-404.js
@@ -0,0 +1,13 @@
+modules.define('page-404', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
