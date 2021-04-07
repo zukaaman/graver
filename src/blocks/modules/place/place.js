f3a4140872fddfcc9e5457d95c3c5e9a65f983dd
zukaaman <zukaaman@gmail.com>
2021-04-08 01:03:17 +0400

    7.4.21

---
+++ src/blocks/modules/place/place.js
@@ -0,0 +1,13 @@
+modules.define('place', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
